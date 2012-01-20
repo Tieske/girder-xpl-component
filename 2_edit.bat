11a6bc024cb2b05eadc795e220374f34dfbda62d
Thijs Schreijer <thijs@thijsschreijer.nl>
2012-01-20 21:00:44 +0100

    missed a Lua file in the edit batch file; "UPnP (xPL).lua", added it.

--- 2_edit.bat
+++ 2_edit.bat
@@ -2,6 +2,7 @@
 echo off
 call x_variables.bat
 start "xPLGirder" "%girdereditor%" "%girder%\luascript\Components\xPL.lua"
+start "xPLGirder" "%girdereditor%" "%girder%\luascript\Components\UPnP*.lua"
 start "xPLGirder" "%girdereditor%" "%girder%\luascript\components\xpl\*.*"
 start "xPLGirder" "%girdereditor%" "%girder%\luascript\components\xpl\support\*.*"
 start "xPLGirder" "%girdereditor%" "%girder%\luascript\components\xpl\Handlers\*.*"
